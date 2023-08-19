44deccbf96c8c2807cfac0fdb2cc5cbf04329c27
Fredaobob <lokgamer01@gmail.com>
2023-08-19 10:41:52 -0300

    Initial commit

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -21,7 +21,7 @@ const SidebarTweetButton = () => {
       <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-green-500
       hover:bg-opacity-90 cursor-pointer transition">
           <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
-            Tweet
+            Login / Register
           </p>
       </div>
     </div>
